1b24dc8d55233ee862a144253063054767acaeb9
Juile Harrow <julie.ann.harrow@gmail.com>
2016-07-18 09:28:35 -0400

    initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      if @comment.save
+        format.html{redirect_to blog_post_path( @comment.blog_post_id), notice: 'Comment was successfully created.'}
+      else
+        format.html { redirect_to blog_post_path}
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author, :comment_entry)
+  end
+end
